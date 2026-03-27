0089518137064486ef995f5da156bb77549fd7c5
Sanidhya Singh <amansanidhya9876@gmail.com>
2026-03-27 12:56:47 +0530

    added mappings in shipment and customer

--- order-inventory/src/main/java/com/spring/order_inventory/entity/Customer.java
+++ order-inventory/src/main/java/com/spring/order_inventory/entity/Customer.java
@@ -1,11 +1,15 @@
 package com.spring.order_inventory.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.List;
 
 @Entity
+@Table(name = "customers")
 @Data
 public class Customer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
@@ -16,4 +20,16 @@ public class Customer {
 
     @Column(name = "full_name")
     private String fullName;
+
+    //RELATIONSHIPS
+
+    //Customer → Orders
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Order> orders;
+
+    //Customer → Shipments
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Shipment> shipments;
 }
