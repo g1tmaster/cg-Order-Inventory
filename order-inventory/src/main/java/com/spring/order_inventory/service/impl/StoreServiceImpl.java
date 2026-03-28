56f1b215313bbe318b8feb55f2429e99f2dd20a6
Ishaan Patel <ishaanpatel29976@gmai.com>
2026-03-28 12:46:20 +0530

    written both APIs for stores entity

--- order-inventory/src/main/java/com/spring/order_inventory/service/impl/StoreServiceImpl.java
+++ order-inventory/src/main/java/com/spring/order_inventory/service/impl/StoreServiceImpl.java
@@ -2,18 +2,22 @@ package com.spring.order_inventory.service.impl;
 
 import com.spring.order_inventory.dto.StoreResponseDto;
 import com.spring.order_inventory.entity.Store;
+import com.spring.order_inventory.repository.OrderRepository;
 import com.spring.order_inventory.repository.StoreRepository;
 import com.spring.order_inventory.service.IStoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.spring.order_inventory.entity.Order;
 import java.util.List;
+import com.spring.order_inventory.dto.OrderResponseDto;
 
 @Service
 @RequiredArgsConstructor
 public class StoreServiceImpl implements IStoreService {
 
     private final StoreRepository storeRepository;
+    private final OrderRepository orderRepository;
+
 
     @Override
     public StoreResponseDto getStoreById(Integer storeId) {
@@ -31,7 +35,19 @@ public class StoreServiceImpl implements IStoreService {
     }
 
     @Override
-    public List<Order> getOrdersByStoreId(Integer storeId) {
-        return List.of(); // placeholder for now
+    public List<OrderResponseDto> getOrdersByStoreId(Integer storeId) {
+        List<Order> orders = orderRepository.findByStoreStoreId(storeId);
+
+        return orders.stream()
+                .map(order -> OrderResponseDto.builder()
+                        .orderId(order.getOrderId())
+                        .orderTms(order.getOrderTms())
+                        .orderStatus(order.getOrderStatus())
+                        .customerId(order.getCustomer().getCustomerId())
+                        .customerName(order.getCustomer().getFullName())
+                        .storeId(order.getStore().getStoreId())
+                        .storeName(order.getStore().getStoreName())
+                        .build())
+                .toList();
     }
 }
