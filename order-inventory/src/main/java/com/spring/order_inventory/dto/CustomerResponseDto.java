c38a418322d366ce7c3d507660ab1870c81b8ecf
g1tmaster <adityaraj4160@gmail.com>
2026-03-28 12:13:01 +0530

    adding builder annotations in dtos

--- order-inventory/src/main/java/com/spring/order_inventory/dto/CustomerResponseDto.java
+++ order-inventory/src/main/java/com/spring/order_inventory/dto/CustomerResponseDto.java
@@ -1,8 +1,10 @@
 package com.spring.order_inventory.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CustomerResponseDto {
 
     private Integer customerId;
--- order-inventory/src/main/java/com/spring/order_inventory/dto/ShipmentResponseDto.java
+++ order-inventory/src/main/java/com/spring/order_inventory/dto/ShipmentResponseDto.java
@@ -1,8 +1,10 @@
 package com.spring.order_inventory.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ShipmentResponseDto {
 
     private Integer shipmentId;
