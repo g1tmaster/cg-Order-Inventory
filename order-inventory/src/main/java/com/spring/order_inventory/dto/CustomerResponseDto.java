576791aa8e980a48b6fa7a26eb815ea7d9efc134
Sanidhya Singh <amansanidhya9876@gmail.com>
2026-03-28 10:42:35 +0530

    added the response dto

--- order-inventory/src/main/java/com/spring/order_inventory/dto/CustomerResponseDto.java
+++ order-inventory/src/main/java/com/spring/order_inventory/dto/CustomerResponseDto.java
@@ -3,7 +3,7 @@ package com.spring.order_inventory.dto;
 import lombok.Data;
 
 @Data
-public class CustomerDTO {
+public class CustomerResponseDto {
 
     private Long customerId;
     private String fullName;
