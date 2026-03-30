03e9b3e1ed8a5f1cceab211f61a757b16f41e27e
Mayank Yadav <mayanksmac@Mayanks-MacBook-Air.local>
2026-03-30 13:36:21 +0530

    Corrected native query for order items

--- order-inventory/src/main/java/com/spring/order_inventory/controller/OrderItemController.java
+++ order-inventory/src/main/java/com/spring/order_inventory/controller/OrderItemController.java
@@ -17,16 +17,16 @@ public class OrderItemController {
 
     @GetMapping("/details")
     public ResponseEntity<List<OrderItemResponseDto>> getAllOrderItemDetails() {
-        return ResponseEntity.ok(orderItemService.getAllOrderItemsWithDetails());
+        return ResponseEntity.ok(orderItemService.getAllOrderItemDetails());
     }
 
     @GetMapping("/shipment/{shipmentId}")
     public ResponseEntity<List<OrderItemResponseDto>> getByShipmentId(@PathVariable Integer shipmentId) {
-        return ResponseEntity.ok(orderItemService.getOrderByShipmentId(shipmentId));
+        return ResponseEntity.ok(orderItemService.getByShipmentId(shipmentId));
     }
 
     @GetMapping("/order/{orderId}")
     public ResponseEntity<List<OrderItemResponseDto>> getByOrderId(@PathVariable Integer orderId) {
-        return ResponseEntity.ok(orderItemService.getOrderItemsByOrderId(orderId));
+        return ResponseEntity.ok(orderItemService.getByOrderId(orderId));
     }
 }
